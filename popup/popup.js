9a82866223abf1b3b59b725ca65a8959478bad95
maltemo <maltemo@protonmail.com>
2022-08-20 00:22:48 +0200

    Fixing bug of duplicates murmurhashes displayed in list

--- popup/popup.js
+++ popup/popup.js
@@ -3,7 +3,8 @@ document
   .addEventListener("click", reloadHashes);
 
 /**
- * Sends a message to the content-scripts to request to compute murmurhashes of favicons
+ * Sends a message to the content-scripts to request to compute murmurhashes of 
+ * favicons
  */
 async function reloadHashes() {
   const active_tab = await getActiveTab();
@@ -24,8 +25,9 @@ browser.runtime.onMessage.addListener(async function (
     if (!message.hashes_list.every((x) => typeof x === "undefined")) {
       document.getElementById("hashes-display").replaceChildren();
     }
-    message.hashes_list.map(addHashInTable);
-    await setIconBadge(message.hashes_list.length);
+    const unique_hashes_list = uniqueHashesAndMergesUrls(message.hashes_list);
+    unique_hashes_list.map(addHashInTable);
+    await setIconBadge(unique_hashes_list.length);
   }
 });
 
@@ -43,7 +45,50 @@ function addHashInTable(hash_data) {
 }
 
 /**
- * Creates a td element and attaches to it a TextNode containing the given string in parameter
+ * Filters out duplicate murmurhashes from the list.
+ * Transfers URL of duplicates in the original one.
+ *
+ * @param {Array.<Object>} hashes_list List of objects that can have duplicated
+ *                         murmurhash property and undefined objects
+ * @return {Array.<Object>} List of objects having unique murmurhashes property
+ */
+function uniqueHashesAndMergesUrls(hashes_list) {
+  const unique_murmurhashes = [];
+  const filtered_hashes_list = [];
+  const duplicated_hashes_list = [];
+
+  hashes_list.forEach(function (data) {
+    // Filters out undefined data
+    if (typeof data === "undefined") return;
+    // Current murmurhash is unique for the moment
+    else if (!unique_murmurhashes.includes(data.murmurhash)) {
+      unique_murmurhashes.push(data.murmurhash);
+      filtered_hashes_list.push(data);
+    }
+    // Murmurhash is already in the unique list
+    else {
+      duplicated_hashes_list.push(data);
+    }
+  });
+
+  // For duplicates, concatenate the url to the first occurence of this murmurhash
+  duplicated_hashes_list.forEach(function (data) {
+    const index = filtered_hashes_list.findIndex(
+      (object) => object.murmurhash === data.murmurhash
+    );
+    // If the url isn't already present in href, then concatenate it to the href
+    if (!filtered_hashes_list[index].href.includes(data.href)) {
+      filtered_hashes_list[
+        index
+      ].href = `${filtered_hashes_list[index].href} - ${data.href}`;
+    }
+  });
+  return filtered_hashes_list;
+}
+
+/**
+ * Creates a td element and attaches to it a TextNode containing the given 
+ * string in parameter
  *
  * @param {string} text Text that will be added in the TextNode inside the td tag
  * @return {Element} Td element with a TextNode attached to it
@@ -58,10 +103,13 @@ function createTextTd(text) {
 }
 
 /**
- * Creates a td element and attaches to it a link referring to shodan murmurhash favicon search
+ * Creates a td element and attaches to it a link referring to shodan
+ * murmurhash favicon search
  *
- * @param {string} murmurhash String containing the murmurhash of an element, in our case of a favicon
- * @return {Element} Td element with a link attached to it, referring to shodan search based on favicon murmurhash
+ * @param {string} murmurhash String containing the murmurhash of an element,
+ *                            in our case of a favicon
+ * @return {Element} Td element with a link attached to it, referring to shodan
+ *                   search based on favicon murmurhash
  */
 function createShodanLinkInTd(murmurhash) {
   const td = document.createElement("td");
@@ -77,7 +125,8 @@ function createShodanLinkInTd(murmurhash) {
 /**
  * Creates a td containing the clipboard icon to copy urls of favicons
  *
- * @param {string} url Url that will be copied on click and stored in title parameter of the image
+ * @param {string} url Url that will be copied on click and stored in title 
+ *                     parameter of the image
  * @return {Element} Td element with an svg image attached to it
  */
 function createURLCopyLink(url) {
@@ -98,11 +147,12 @@ function createURLCopyLink(url) {
   return td;
 }
 
-
 /**
- * Sets text, color and background color of the notification badge on extension logo in toolbar
+ * Sets text, color and background color of the notification badge on extension
+ * logo in toolbar
  *
- * @param {string} number_of_hashes_found Number of hash found on the page that will be displayed on the badge
+ * @param {string} number_of_hashes_found Number of hash found on the page that
+ *                                        will be displayed on the badge
  */
 async function setIconBadge(number_of_hashes_found) {
   const active_tab = await getActiveTab();
@@ -123,7 +173,8 @@ async function setIconBadge(number_of_hashes_found) {
 }
 
 /**
- * As browser.tabs.getCurrent() is never available in the scope, getting current tab this way
+ * As browser.tabs.getCurrent() is never available in the scope, getting
+ * current tab this way
  *
  * @return {tabs.Tab} Active tab
  **/
@@ -135,7 +186,7 @@ async function getActiveTab() {
 /**
  * Toggles visibility of an element of the DOM
  *
- * @param {Element} tag DOM Element having one of those two classes : visible or hidden
+ * @param {Element} tag DOM Element having only one of those classes : visible or hidden
  */
 function toggleVisibility(tag) {
   // XOR -> if tag has hidden class or visible class but not the two simultaneously
